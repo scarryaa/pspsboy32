e88273b968e637b7dd780c3bbdfba62f02a9507f
Scarlet <scarryaa@gmail.com>
2023-12-17 13:27:04 -0500

    desktop impl working

--- src/file-reader/desktop-file-reader.h
+++ src/file-reader/desktop-file-reader.h
@@ -1,42 +1,74 @@
-#include <fstream>
 #include "file-reader.h"
+#include <cstdio>
+#include <cstring>
+#include <iostream>
 
 #ifndef __DESKTOP_FILE_READER_H__
 #define __DESKTOP_FILE_READER_H__
 
 class DesktopFileReader : public FileReader
 {
-    std::fstream file;
+    FILE *file;
 
 public:
+    DesktopFileReader() : file(nullptr) {}
+
+    ~DesktopFileReader()
+    {
+        if (file)
+        {
+            fclose(file);
+        }
+    }
+
     bool open(const std::string &path) override
     {
-        file.open(path, std::ios::binary | std::ios::in | std::ios::out);
-        return file.is_open();
+        // Open for reading and writing in binary mode
+        file = fopen(path.c_str(), "rb+");
+        if (!file)
+        {
+            std::cerr << "Failed to open file: " << path << std::endl;
+        }
+        return file != nullptr;
     }
 
     size_t read(char *buffer, size_t size) override
     {
-        if (!file.read(buffer, size))
+        if (!file)
         {
-            return file.gcount();
+            std::cerr << "File not open for reading." << std::endl;
+            return 0;
         }
-        return size;
+
+        size_t bytesRead = fread(buffer, 1, size, file);
+        if (bytesRead != size && ferror(file))
+        {
+            std::cerr << "Read error occurred." << std::endl;
+        }
+        return bytesRead;
     }
 
     void write(std::string value) override
     {
-        if (file.is_open())
+        if (!file)
         {
-            file << value;
+            std::cerr << "File not open for writing." << std::endl;
+            return;
+        }
+
+        size_t bytesWritten = fwrite(value.c_str(), 1, value.size(), file);
+        if (bytesWritten != value.size())
+        {
+            std::cerr << "Write error occurred." << std::endl;
         }
     }
 
     void close() override
     {
-        if (file.is_open())
+        if (file)
         {
-            file.close();
+            fclose(file);
+            file = nullptr;
         }
     }
 };
